2452fc4d3c79f7060aab0c100dd7afe4c603b9a5
Keshara Weerasinghe <keshara2032@gmail.com>
2025-01-02 16:11:21 -0500

    init next.js app

---
+++ uva-dsa/src/app/components/header.tsx
@@ -0,0 +1,20 @@
+import styles from '../styles/header.module.css';
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>UVA DSA</div>
+      <nav>
+        <ul className={styles.navLinks}>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#people">People</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#resources">Resources</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ uva-dsa/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from './components/header';
+import Content from './components/content';
+import Footer from './components/footer';
+
+export default function HomePage() {
+  return (
+    <div className='container'>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
